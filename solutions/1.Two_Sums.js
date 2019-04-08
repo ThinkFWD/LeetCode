cddc5bf34663e2ba536b8d408c7b778422b17c73
thinkFWD <mike.thinkfwd@gmail.com>
2019-04-07 23:03:29 -0400

    Setup for new beginning

---
+++ solutions/1.Two_Sums.js
@@ -0,0 +1,34 @@
+/* 
+++++++ QUESTION ++++++ 
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+++++++ EXAMPLE ++++++
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+++++++ ANALYSIS ++++++
+
+Assumption:
+  Each input would only have 1 solution
+
+Restriction:
+  You cannot use the same element twice
+
+*/
+/*--------------------------------------------------------------------------------------*/
+/**
+ * 
+ * @param {number[]} nums - Series of numbers
+ * @param {number} target - Single number
+ * @return {number[]}
+ * 
+ */
+
+//Brute Force Solution 
+const twoSum = (nums, target) => {
+  let solution = [1,2];
+  return solution;
+};
